4b65d9ef8a6d92af91ac54ec6833d2aa2e980f5f
DavidMukhiya <sherlock.41v94@gmail.com>
2022-04-17 16:58:20 +0545

    Added Seven Button to play Seven Sound

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,79 @@ class XylophoneApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play("note1.wav");
-              },
-              child: Text("Click Me"),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red),
+                child: Container(
+                  color: Colors.red,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play("note1.wav");
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                child: Container(
+                  color: Colors.blue,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play("note2.wav");
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                child: Container(
+                  color: Colors.orange,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play("note3.wav");
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                child: Container(
+                  color: Colors.purple,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play("note4.wav");
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                child: Container(
+                  color: Colors.yellow,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play("note5.wav");
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green),
+                child: Container(
+                  color: Colors.green,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play("note6.wav");
+                },
+              ),
+              TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.lime),
+                child: Container(
+                  color: Colors.lime,
+                ),
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play("note7.wav");
+                },
+              ),
+            ],
           ),
         ),
       ),
